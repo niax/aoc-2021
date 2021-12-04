a1d641fbba5265539dd858e7595874ea523cb977
Nicholas Hollett <niax@niax.co.uk>
2021-12-04 13:04:55 +0000

    Solve day 4 part 1

---
+++ day-04/src/main.rs
@@ -0,0 +1,105 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+#[derive(Debug, Clone)]
+struct BingoBoard {
+    nums: Vec<Vec<u32>>,
+    marks: Vec<Vec<bool>>,
+}
+
+const BOARD_SIZE: usize = 5;
+
+impl BingoBoard {
+    pub fn new(nums: Vec<Vec<u32>>) -> Self {
+        Self {
+            nums,
+            marks: vec![vec![false; BOARD_SIZE]; BOARD_SIZE],
+        }
+    }
+
+    pub fn at(&self, x: usize, y: usize) -> (u32, bool) {
+        (self.nums[y][x], self.marks[y][x])
+    }
+
+    pub fn mark(&mut self, num: u32) {
+        for (y, row) in self.nums.iter().enumerate() {
+            for (x, n) in row.iter().enumerate() {
+                if *n == num {
+                    self.marks[y][x] = true;
+                }
+            }
+        }
+    }
+
+    pub fn wins(&self) -> bool {
+        // Check Rows
+        let rows_win = self
+            .marks
+            .iter()
+            .map(|row| row.iter().map(|c| if *c { 1 } else { 0 }).sum::<usize>() == BOARD_SIZE)
+            .any(|r| r);
+
+        let columns_win = (0..BOARD_SIZE)
+            .map(|x| {
+                (0..BOARD_SIZE)
+                    .map(|y| if self.marks[y][x] { 1 } else { 0 })
+                    .sum::<usize>()
+                    == BOARD_SIZE
+            })
+            .any(|c| c);
+
+        rows_win || columns_win
+    }
+}
+
+fn main() {
+    let file = File::open("input.txt").expect("Could not open input file");
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+    let called_numbers: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mut boards = Vec::new();
+    let mut cur_board = Vec::new();
+    for line_res in lines {
+        let line = line_res.unwrap();
+        if line.is_empty() {
+            boards.push(BingoBoard::new(cur_board));
+            cur_board = Vec::new();
+        } else {
+            cur_board.push(
+                line.split_whitespace()
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect(),
+            )
+        }
+    }
+
+    // Part 1
+    let mut winner = None;
+    let mut called = 0;
+    'outer: for num in called_numbers {
+        called = num;
+        for board in boards.iter_mut() {
+            board.mark(num);
+            if board.wins() {
+                winner = Some(board.clone());
+                break 'outer;
+            }
+        }
+    }
+
+    let unmarked_sum: u32 = (0..BOARD_SIZE)
+        .map(|y| 
+            (0..BOARD_SIZE).map(|x| winner.as_ref().unwrap().at(x, y)).filter(|(_, mark)| !mark).map(|(n, _)| n).sum::<u32>()
+        ).sum();
+
+
+    println!("{}", unmarked_sum * called);
+}
