e73c9715a4f4eb7c17bb6ccf7b85cb5fd5ebd1da
Nicholas Hollett <niax@niax.co.uk>
2021-12-09 13:47:55 +0000

    Solve day 9 part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,14 +1,11 @@
 use commons::grid::{Grid, SparseGrid};
 use commons::io::load_file_lines;
+use itertools::Itertools;
+use std::collections::{HashSet, VecDeque};
 
 fn main() {
     let mut grid = SparseGrid::new();
-    let adjacent: Vec<(isize, isize)> = vec![
-        (0, -1),
-        (0, 1),
-        (1, 0),
-        (-1, 0),
-    ];
+    let adjacent: Vec<(isize, isize)> = vec![(0, -1), (0, 1), (1, 0), (-1, 0)];
 
     for (y, line) in load_file_lines::<String>("input.txt").enumerate() {
         for (x, c) in line.unwrap().chars().enumerate() {
@@ -19,10 +16,11 @@ fn main() {
         }
     }
 
-    let mut sum = 0;
+    let mut low_points = Vec::new();
     for y in 0..grid.height() {
         for x in 0..grid.width() {
-            let value = grid.at(&(x as isize, y as isize)).unwrap();
+            let coord = (x as isize, y as isize);
+            let value = grid.at(&coord).unwrap();
             let surrounding_greater = adjacent
                 .iter()
                 .map(|(x1, y1)| {
@@ -33,9 +31,43 @@ fn main() {
                 .filter(|v1| v1 > value)
                 .count();
             if surrounding_greater == 4 {
-                sum += *value as u32 + 1;
+                low_points.push(coord);
             }
         }
     }
-    println!("{}", sum);
+    let part1 = low_points
+        .iter()
+        .map(|c| *grid.at(c).unwrap() as u32 + 1)
+        .sum::<u32>();
+    println!("{}", part1);
+
+    let mut sizes = Vec::new();
+    for point in low_points {
+        let mut to_search = VecDeque::from([point]);
+        let mut seen = HashSet::new();
+        let mut size = 0;
+        while !to_search.is_empty() {
+            let coord = to_search.pop_front().unwrap();
+            let maybe_value = grid.at(&coord);
+            if seen.contains(&coord) || maybe_value.is_none() {
+                continue;
+            }
+            let value = *maybe_value.unwrap();
+            if value == 9 {
+                continue;
+            }
+            size += 1;
+
+            let (x, y) = coord;
+            for &(x1, y1) in &adjacent {
+                let next = (x + x1, y + y1);
+                to_search.push_back(next);
+            }
+
+            seen.insert(coord);
+        }
+        sizes.push(size);
+    }
+    let part2 = sizes.iter().sorted().rev().take(3).product::<u32>();
+    println!("{:?}", part2);
 }
