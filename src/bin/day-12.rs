db312d625674685c4d8ef6a886f38b2b46c8c308
Nicholas Hollett <niax@niax.co.uk>
2021-12-12 14:47:51 +0000

    Solve day 12 part 2

--- src/bin/day-12.rs
+++ src/bin/day-12.rs
@@ -1,50 +1,46 @@
 use aoc2021::commons::io::load_stdin_lines;
+use petgraph::graph::{Graph, NodeIndex, UnGraph};
 use std::collections::{HashMap, HashSet};
-use petgraph::graph::{NodeIndex, Graph, UnGraph};
 
-#[derive(Debug,Hash, Clone, Eq, PartialEq)]
+#[derive(Debug, Hash, Clone, Eq, PartialEq)]
 struct Cave {
     name: String,
-    small: bool
+    small: bool,
 }
 
 impl Cave {
     pub fn parse(s: String) -> Self {
         let small = s.to_lowercase() == s;
 
-        Self {
-            name: s,
-            small,
-        }
+        Self { name: s, small }
     }
 }
 
-
-fn find_paths(graph: &UnGraph<Cave, ()>, current: NodeIndex<u32>, path: Vec<Cave>, visited_small: HashSet<Cave>) -> u32{
-    let mut new_path = path.clone();
-    new_path.push(graph.node_weight(current).unwrap().clone());
-    let mut valid_paths = 0;
+fn find_paths(
+    graph: &UnGraph<Cave, ()>,
+    current: NodeIndex<u32>,
+    visited_small: HashSet<Cave>,
+    visisted_twice: bool,
+) -> u32 {
     if graph.node_weight(current).unwrap().name == "end" {
-        let mut s = String::new();
-        for n in new_path {
-            s += &n.name;
-            s.push(',');
-        }
-        //println!("{}", s);
-        return 1;
+        1
+    } else {
+        graph.neighbors(current).map( |neigh| {
+            let mut inner_twice = visisted_twice;
+            let mut new_visited = visited_small.clone();
+            let cave = graph.node_weight(neigh).unwrap();
+            if cave.small {
+                if visited_small.contains(cave) {
+                    if inner_twice || cave.name == "start" {
+                        return 0;
+                    }
+                    inner_twice = true;
+                }
+                new_visited.insert(cave.clone());
+            }
+            find_paths(graph, neigh, new_visited, inner_twice)
+        }).sum()
     }
-    for neigh in graph.neighbors(current) {
-        let mut new_visited = visited_small.clone();
-        let cave = graph.node_weight(neigh).unwrap();
-        if cave.small {
-            if visited_small.contains(cave) {
-                continue;
-            } 
-            new_visited.insert(cave.clone());
-        }
-        valid_paths += find_paths(graph, neigh, new_path.clone(), new_visited);
-    }
-    return valid_paths;
 }
 
 fn main() {
@@ -53,12 +49,12 @@ fn main() {
     for line in load_stdin_lines::<String>() {
         let line = line.unwrap();
         let (a, b) = line.split_once('-').unwrap();
-        let a_node = node_ids.entry(a.to_string()).or_insert_with(
-            || graph.add_node(Cave::parse(a.to_string()))
-        ).clone();
-        let b_node = node_ids.entry(b.to_string()).or_insert_with(
-            || graph.add_node(Cave::parse(b.to_string()))
-        ).clone();
+        let a_node = *node_ids
+            .entry(a.to_string())
+            .or_insert_with(|| graph.add_node(Cave::parse(a.to_string())));
+        let b_node = *node_ids
+            .entry(b.to_string())
+            .or_insert_with(|| graph.add_node(Cave::parse(b.to_string())));
 
         graph.add_edge(a_node, b_node, ());
     }
@@ -66,6 +62,9 @@ fn main() {
     let start = node_ids["start"];
     let mut visited = HashSet::new();
     visited.insert(graph.node_weight(start).unwrap().clone());
-    let part1 = find_paths(&graph, start, Vec::new(), visited);
+    let part1 = find_paths(&graph, start, visited.clone(), true);
     println!("{}", part1);
+
+    let part2 = find_paths(&graph, start, visited, false);
+    println!("{}", part2);
 }
