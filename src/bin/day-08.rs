62480c134581795f512a15577d830fd61272d159
Nicholas Hollett <niax@niax.co.uk>
2021-12-13 00:06:41 +0000

    Make day 08 faster by using bits

--- src/bin/day-08.rs
+++ src/bin/day-08.rs
@@ -1,25 +1,26 @@
 use aoc2021::commons::io::load_stdin_lines;
-use itertools::Itertools;
 use lazy_static::lazy_static;
-use hashbrown::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::str::FromStr;
 use thiserror::Error;
 
 lazy_static! {
-    static ref DIGITS: Vec<&'static str> = vec![
-        "ABCEFG", "CF", "ACDEG", "ACDFG", "BCDF", "ABDFG", "ABDEFG", "ACF", "ABCDEFG", "ABCDFG",
-    ];
+    static ref DIGITS: Vec<u32> = vec![6, 2, 5, 5, 4, 5, 6, 3, 7, 6,];
 }
 
 #[derive(Debug)]
 struct PuzzleInput {
-    digits: Vec<HashSet<char>>,
-    wanted_numbers: Vec<HashSet<char>>,
+    digits: Vec<u8>,
+    wanted_numbers: Vec<u8>,
     solution: Option<Vec<u8>>,
 }
 
+fn bit_difference(a: u8, b: u8) -> u32 {
+    (a & (a ^ b)).count_ones()
+}
+
 impl PuzzleInput {
-    pub fn new(digits: Vec<HashSet<char>>, wanted_numbers: Vec<HashSet<char>>) -> Self {
+    pub fn new(digits: Vec<u8>, wanted_numbers: Vec<u8>) -> Self {
         Self {
             digits,
             wanted_numbers,
@@ -28,59 +29,64 @@ impl PuzzleInput {
     }
 
     fn actually_solve(&mut self) {
-        let mut digit_to_wires = HashMap::new();
+        let mut digit_to_wires = vec![0; 10];
 
         let digits = self.digits.clone();
         for i in &[1, 4, 7, 8] {
-            let digit = digits.iter().find(|s| s.len() == DIGITS[*i].len()).unwrap();
-            digit_to_wires.insert(*i as u8, digit.clone());
+            let digit = digits
+                .iter()
+                .find(|s| s.count_ones() == DIGITS[*i])
+                .unwrap();
+            digit_to_wires[*i] = *digit;
         }
-        let one = digit_to_wires[&1].clone();
-        let five_long: Vec<_> = digits.iter().filter(|s| s.len() == 5).collect();
-
-        let (threes, five_long): (Vec<&HashSet<char>>, Vec<&HashSet<char>>) = five_long
+        let one = digit_to_wires[1];
+        let five_long: Vec<u8> = digits
             .iter()
-            .partition(|s| s.difference(&one).count() == 3);
-        let three = threes[0];
-        digit_to_wires.insert(3, three.clone());
+            .filter(|s| s.count_ones() == 5)
+            .copied()
+            .collect();
+
+        let (threes, five_long): (Vec<&u8>, Vec<&u8>) = five_long
+            .iter()
+            .partition(|s| bit_difference(**s, one) == 3);
+        let three = *threes[0];
+        digit_to_wires[3] = three;
 
         let six = {
-            let six_long: Vec<_> = digits.iter().filter(|s| s.len() == 6).collect();
-            let (sixes, others): (Vec<&HashSet<char>>, Vec<&HashSet<char>>) = six_long
+            let six_long: Vec<_> = digits
                 .iter()
-                .partition(|s| s.difference(&one).count() == 5);
-            let six = sixes[0];
-            digit_to_wires.insert(6, six.clone());
+                .filter(|s| s.count_ones() == 6)
+                .copied()
+                .collect();
+            let (sixes, others): (Vec<&u8>, Vec<&u8>) =
+                six_long.iter().partition(|s| bit_difference(**s, one) == 5);
+            let six = *sixes[0];
+            digit_to_wires[6] = six;
 
-            let (nines, zeros): (Vec<&HashSet<char>>, Vec<&HashSet<char>>) = others
+            let (nines, zeros): (Vec<&u8>, Vec<&u8>) = others
                 .iter()
-                .partition(|s| s.difference(three).count() == 1);
-            let nine = nines[0];
-            digit_to_wires.insert(9, nine.clone());
-            let zeros = zeros[0];
-            digit_to_wires.insert(0, zeros.clone());
+                .partition(|s| bit_difference(***s, three) == 1);
+            digit_to_wires[9] = *nines[0];
+            digit_to_wires[0] = *zeros[0];
 
             six
         };
 
-        let (twos, fives): (Vec<&HashSet<char>>, Vec<&HashSet<char>>) = five_long
+        let (twos, fives): (Vec<&u8>, Vec<&u8>) = five_long
             .iter()
-            .partition(|s| s.difference(six).count() == 1);
-        digit_to_wires.insert(2, twos[0].clone());
-        digit_to_wires.insert(5, fives[0].clone());
+            .partition(|s| bit_difference(***s, six) == 1);
+        digit_to_wires[2] = *twos[0];
+        digit_to_wires[5] = *fives[0];
 
         let mut wires_to_digit = HashMap::new();
-        for (k, v) in digit_to_wires {
-            let s = v.iter().sorted().collect::<String>();
-            wires_to_digit.insert(s, k);
+        for (k, v) in digit_to_wires.iter().enumerate() {
+            wires_to_digit.insert(v, k);
         }
 
         self.solution = Some(
             self.wanted_numbers
                 .iter()
-                .map(|s| s.iter().sorted().collect::<String>())
-                .map(|s| wires_to_digit.get(&s).unwrap_or(&u8::MAX))
-                .copied()
+                .map(|s| wires_to_digit.get(s).map(|s| *s as u8).unwrap_or(u8::MAX))
                 .collect(),
         );
     }
@@ -110,10 +116,16 @@ impl FromStr for PuzzleInput {
         let mut seen_sep = false;
 
         for word in input_string.split_whitespace() {
-            let letter_set: HashSet<char> = word.chars().collect();
             if word == "|" {
                 seen_sep = true;
-            } else if !seen_sep {
+                continue;
+            }
+            let mut letter_set = 0;
+            for c in word.chars() {
+                let offset = c as usize - 'a' as usize;
+                letter_set |= 1 << offset;
+            }
+            if !seen_sep {
                 digits.push(letter_set);
             } else {
                 wanted_numbers.push(letter_set);
