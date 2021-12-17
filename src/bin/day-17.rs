e95ec03972e2aeae56fdec4cd35b91ee5fe3b5fb
Nicholas Hollett <niax@niax.co.uk>
2021-12-17 22:30:39 +0000

    Reduce the search space in day 17

--- src/bin/day-17.rs
+++ src/bin/day-17.rs
@@ -19,40 +19,76 @@ fn step(pos: Vec2, velocity: Vec2) -> (Vec2, Vec2) {
     (new_pos, new_velocity)
 }
 
-fn test_y(mut dy: isize, y_range: &RangeInclusive<isize>, min_y: isize) -> bool {
-    let mut pos = 0;
-    while pos >= min_y {
-        if y_range.contains(&pos) {
-            return true;
-        }
-        pos += dy;
-        dy -= 1;
-    }
-
-    false
+#[allow(dead_code)]
+struct Finder {
+    x_range: RangeInclusive<isize>,
+    y_range: RangeInclusive<isize>,
+    min_x: isize,
+    max_x: isize,
+    min_y: isize,
+    max_y: isize,
 }
 
-fn test_velocity(
-    mut velocity: Vec2,
-    x_range: &RangeInclusive<isize>,
-    y_range: &RangeInclusive<isize>,
-) -> Option<isize> {
-    let mut pos = (0, 0);
-    let mut max_height = isize::MIN;
-    let min_y = *y_range.start().min(y_range.end());
-    let max_x = *x_range.start().max(x_range.end());
-    while pos.0 <= max_x && pos.1 >= min_y {
-        max_height = max_height.max(pos.1);
-
-        if x_range.contains(&pos.0) && y_range.contains(&pos.1) {
-            return Some(max_height);
+impl Finder {
+    pub fn new(x1: isize, x2: isize, y1: isize, y2: isize) -> Self {
+        let min_x = x1.min(x2);
+        let max_x = x1.max(x2);
+        let min_y = y1.min(y2);
+        let max_y = y1.max(y2);
+        let x_range = min_x..=max_x;
+        let y_range = min_y..=max_y;
+        Self {
+            x_range,
+            y_range,
+            min_x,
+            max_x, 
+            min_y,
+            max_y,
         }
-        let (new_pos, new_velocity) = step(pos, velocity);
-        pos = new_pos;
-        velocity = new_velocity;
     }
 
-    None
+    pub fn test_x(&self, mut dx: isize) -> bool {
+        let mut pos = 0;
+        while pos <= self.max_x && dx > 0{
+            if self.x_range.contains(&pos) {
+                return true;
+            }
+            pos += dx;
+            dx = if dx == 0 { 0 } else { dx - 1 };
+        }
+
+        false
+    }
+
+    pub fn test_y(&self, mut dy: isize) -> bool {
+        let mut pos = 0;
+        while pos >= self.min_y {
+            if self.y_range.contains(&pos) {
+                return true;
+            }
+            pos += dy;
+            dy -= 1;
+        }
+
+        false
+    }
+
+    fn test_velocity(&self, mut velocity: Vec2) -> Option<isize> {
+        let mut pos = (0, 0);
+        let mut max_height = isize::MIN;
+        while pos.0 <= self.max_x && pos.1 >= self.min_y {
+            max_height = max_height.max(pos.1);
+
+            if self.x_range.contains(&pos.0) && self.y_range.contains(&pos.1) {
+                return Some(max_height);
+            }
+            let (new_pos, new_velocity) = step(pos, velocity);
+            pos = new_pos;
+            velocity = new_velocity;
+        }
+
+        None
+    }
 }
 
 fn main() {
@@ -62,21 +98,19 @@ fn main() {
     let x2 = caps.get(2).unwrap().as_str().parse::<isize>().unwrap();
     let y1 = caps.get(3).unwrap().as_str().parse::<isize>().unwrap();
     let y2 = caps.get(4).unwrap().as_str().parse::<isize>().unwrap();
-    let x_range = x1..=x2;
-    let y_range = y1..=y2;
 
-    let min_y = *y_range.start().min(y_range.end());
-    let y = y_range.end().abs();
+    let finder = Finder::new(x1, x2, y1, y2);
 
-    let valid_y: Vec<_> = (-y * 4..y * 4)
-        .filter(|p| test_y(*p, &y_range, min_y))
+    let y = finder.max_y.abs()*2;
+    let valid_y: Vec<_> = (-y..=y)
+        .filter(|p| finder.test_y(*p))
         .collect();
 
     let mut max_height = isize::MIN;
     let mut possible_count = 0;
-    for dx in 1..=x2 {
+    for dx in (1..=x2).filter(|x| finder.test_x(*x)) {
         for dy in &valid_y {
-            if let Some(height) = test_velocity((dx, *dy), &x_range, &y_range) {
+            if let Some(height) = finder.test_velocity((dx, *dy)) {
                 max_height = std::cmp::max(max_height, height);
                 possible_count += 1;
             }
