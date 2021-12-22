45ee98f7845b1536a0698d8301fedde706dc9523
Nicholas Hollett <niax@niax.co.uk>
2021-12-22 16:23:29 +0000

    Mutate lists inline instead of copying for reduce in day 18.

--- src/bin/day-18.rs
+++ src/bin/day-18.rs
@@ -23,21 +23,19 @@ impl Num {
         Num { d }
     }
 
-    fn reduce_inner(&self) -> (Num, bool) {
-        let mut new: Vec<NumElem> = self.d.clone();
-
+    fn reduce_inner(num: &mut Vec<NumElem>) -> bool {
         let mut has_acted = false;
-        for i in 0..new.len() {
-            let sub = &new[i];
+        for i in 0..num.len() {
+            let sub = &num[i];
             if sub.depth == 5 {
-                let (left, right) = (new[i].n, new[i + 1].n);
-                new[i] = NumElem { depth: 4, n: 0 };
-                new.remove(i + 1);
+                let (left, right) = (num[i].n, num[i + 1].n);
+                num[i] = NumElem { depth: 4, n: 0 };
+                num.remove(i + 1);
                 if i != 0 {
-                    let before = new.get_mut(i - 1).unwrap();
+                    let before = num.get_mut(i - 1).unwrap();
                     before.n += left;
                 }
-                if let Some(after) = new.get_mut(i + 1) {
+                if let Some(after) = num.get_mut(i + 1) {
                     after.n += right;
                 }
                 has_acted = true;
@@ -46,13 +44,13 @@ impl Num {
         }
 
         if !has_acted {
-            for i in 0..new.len() {
-                let sub = &new[i];
+            for i in 0..num.len() {
+                let sub = &num[i];
                 if sub.n >= 10 {
                     let n = sub.n;
                     let depth = sub.depth + 1;
-                    new[i] = NumElem { depth, n: (n / 2) };
-                    new.insert(
+                    num[i] = NumElem { depth, n: (n / 2) };
+                    num.insert(
                         i + 1,
                         NumElem {
                             depth,
@@ -65,18 +63,16 @@ impl Num {
             }
         }
 
-        (Num { d: new }, has_acted)
+        has_acted
     }
 
     pub fn reduce(&self) -> Num {
-        let mut result = self.clone();
+        let mut result = self.d.clone();
         let mut modified = true;
         while modified {
-            let t = result.reduce_inner();
-            result = t.0;
-            modified = t.1;
+            modified = Num::reduce_inner(&mut result);
         }
-        result
+        Num { d: result }
     }
 
     fn mag_inner(&self, i: &mut usize, depth: u16) -> u32 {
